541efafef1b14c17420e3bf35ced3b2a39c93e2e
ryantesoro <ryantesoro@yahoo.com>
2020-02-13 14:07:11 +0800

    [:sparkles:] Added navigation routes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+//Login Page
+Route::get('/', ['as' => 'login', 'uses' => 'HomeController@login']);
+
+//Login Process
+Route::post('login', ['as' => 'admin.login', 'uses' => 'UserController@login']);
+
+//When Logged In
+Route::group(['middleware' => 'auth'], function() {
+
+    //Dashboard Page
+    Route::get('dashboard', ['as' => 'admin.dashboard', 'uses' => 'HomeController@dashboard']);
+
+    //Orders Page
+    Route::get('orders', ['as' => 'admin.order.index', 'uses' => 'OrderController@index']);
+
+    //Products Page
+    Route::get('products', ['as' => 'admin.product.index', 'uses' => 'ProductController@index']);
+
+    //Customers Page
+    Route::get('customers', ['as' => 'admin.customer.index', 'uses' => 'CustomerController@index']);
+
+    //Sales Page
+    Route::get('sales', ['as' => 'admin.sales.index', 'uses' => 'SalesController@index']);
+
+    //Reports Page
+    Route::get('reports', ['as' => 'admin.report.index', 'uses' => 'ReportController@index']);
+
+    //Sign Out
+    Route::get('signout', ['as' => 'admin.signout', 'uses' => 'UserController@sign_out']);
 });
-
-Route::post('login', ['as' => 'admin.login', 'uses' => 'UsersController@login']);
-
-
-Route::get('dashboard', ['as' => 'admin.dashboard', 'uses' => 'UsersController@dashboard']);
