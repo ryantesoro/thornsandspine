df02f2916b6066fe6fbfe3325e235a4416aeb7ab
ryantesoro <ryantesoro@yahoo.com>
2020-02-13 14:04:13 +0800

    [:sparkles:] Sign out process

---
+++ app/Http/Controllers/WebController/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\WebController;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use RealRashid\SweetAlert\Facades\Alert;
+
+
+class UserController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function login(Request $request) 
+    {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        $email = $request->get('login_email');
+        $password = $request->get('login_password');
+
+        $credentials = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $login_attempt = auth()->attempt($credentials);
+
+        if (!$login_attempt) {
+            $this->incrementLoginAttempts($request);
+            Alert::error('Login Failed', 'Invalid Email/Password');
+            return redirect()->back()->withInput();
+        }
+        
+        return redirect()->route('admin.dashboard');
+    }
+
+    public function sign_out(Request $request)
+    {
+        auth()->logout();
+        return redirect()->route('login');
+    }
+}
