6b431d9aadf915559b3d90295498712441f14416
ryantesoro <ryantesoro@yahoo.com>
2020-02-14 13:59:32 +0800

    [:sparkles:] Added photo controller

---
+++ app/Http/Controllers/WebController/PhotoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\WebController;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class PhotoController extends Controller
+{
+    public function show(Request $request, $image_name)
+    {
+        $file_path = "product/";
+        if ($request->has('size')) {
+            $size = $request->get('size');
+            
+            if ($size == "medium") {
+                $file_path = "product/medium/";
+            } else if ($size == "thumbnail") {
+                $file_path = "product/thumbnail/";
+            }
+        }
+
+        $image = Storage::disk('local')->get($file_path.$image_name);
+        return response()->make($image, 200, ['Content-Type' => 'Image']);
+    }
+}
