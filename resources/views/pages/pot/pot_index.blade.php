648e1ae25598b20a42cb1c90e53360c12f0e8642
ryantesoro <ryantesoro@yahoo.com>
2020-02-17 16:39:58 +0800

    [:lipstick:] added pot index ui

---
+++ resources/views/pages/pot/pot_index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.master')
+@section('header_name', 'Pots')
+@section('content')
+@include('layouts.nav')
+
+<div class="container-fluid">
+  @include('layouts.sidebar')
+  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    @include('layouts.header')
+    <div class="row">
+      <div class="col text-left">
+        <h4>List Of Pots</h4>
+      </div>
+      <div>
+        <a class="btn btn-success font-weight-bold" href="{{ route('admin.product.create') }}">Create Pot</a>
+      </div>
+    </div>
+    <hr>
+    <div class="table-responsive">
+      <table class="table table-hover border">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>{{$product['name']}}</td>
+            <td>{{$product['price']}}</td>
+            <td class="pt-3">
+              @if (empty($product['deleted_at']))
+                <span class="badge badge-pill badge-info">shown</span>
+              @else
+                <span class="badge badge-pill badge-secondary">hidden</span>
+              @endif
+            </td>
+            <td>
+              <a href="{{ route('admin.product.show', $product['code']) }}"
+                class="btn btn-sm btn-primary font-weight-bold">View</a>
+              <a href="{{ route('admin.product.edit', $product['code']) }}"
+                class="btn btn-sm btn-warning font-weight-bold">Edit</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </main>
+</div>
+</div>
+@endsection
