2643ac91a6c18325a34bea3573b4c36bd2b011d4
ryantesoro <ryantesoro@yahoo.com>
2020-02-17 14:56:57 +0800

    [:hammer_and_wrench:] added order index to controller

--- app/Http/Controllers/ApiController/OrderController.php
+++ app/Http/Controllers/ApiController/OrderController.php
@@ -7,8 +7,23 @@ use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
+    private $user_id;
+
     public function index(Request $request)
     {
+        $this->setUserId(auth()->user()->id);
         
+        $customer = $this->customer()->getCustomerDetailsByUser($this->user_id);
+        $orders = $this->customer()->getCustomerOrders($customer)->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $orders
+        ]);
+    }
+
+    private function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
     }
 }
