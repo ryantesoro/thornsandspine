683fde48be061533d679bc462b007089eef1837b
ryantesoro <ryantesoro@yahoo.com>
2020-02-17 10:44:26 +0800

    [:sparkles:] added cart tables

---
+++ database/migrations/2020_02_17_023622_create_cart_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product', function (Blueprint $table) {
+            $table->unsignedInteger('cart_id');
+            $table->unsignedInteger('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_product');
+    }
+}
