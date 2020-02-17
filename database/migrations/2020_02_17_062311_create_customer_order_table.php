6298e01dc2eb2cc23344fe8da00a81443b885023
ryantesoro <ryantesoro@yahoo.com>
2020-02-17 14:26:16 +0800

    [:sparkles:] connected customer to order table

---
+++ database/migrations/2020_02_17_062311_create_customer_order_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_order', function (Blueprint $table) {
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('order_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_order');
+    }
+}
