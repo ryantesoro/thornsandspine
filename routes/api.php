3991196a2dc3f447b1d128eed0d4f76a54941fa7
ryantesoro <ryantesoro@yahoo.com>
2020-02-17 10:41:56 +0800

    [:sparkles:] added browse and show to customer

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,13 @@ Route::post('password/reset', ['as' => 'password.reset', 'uses' => 'ResetPasswor
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('user', ['as' => 'user', 'uses' => 'UserController@user']);
     Route::get('test', ['as' => 'test', 'uses' => 'UserController@test']);
+
+    //PRODUCTS
+    Route::group(['prefix' => 'product'], function() {
+        //Browse Products
+        Route::get('browse', ['as' => 'product.index', 'uses' => 'ProductController@index']);
+
+        //Show Product
+        Route::get('{code}', ['as' => 'product.show', 'uses' => 'ProductController@show']);
+    });
 });
