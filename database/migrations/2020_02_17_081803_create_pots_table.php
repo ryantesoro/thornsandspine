8ee4cdac93da3a680e896edb688739917ed19fd8
ryantesoro <ryantesoro@yahoo.com>
2020-02-18 10:44:04 +0800

    [:hammer_and_wrench:] removed soft delete

--- database/migrations/2020_02_17_081803_create_pots_table.php
+++ database/migrations/2020_02_17_081803_create_pots_table.php
@@ -17,6 +17,7 @@ class CreatePotsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
