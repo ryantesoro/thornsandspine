820b55bcdd6e3de48391a6d64e863c84d2ef2b7f
ryantesoro <ryantesoro@yahoo.com>
2020-02-18 14:57:56 +0800

    [:hammer_and_wrench:] added store shipping fee

--- app/ShippingFee.php
+++ app/ShippingFee.php
@@ -15,4 +15,26 @@ class ShippingFee extends Model
     ];
 
     public $timestamps = false;
+
+    //Fetch all shipping fees
+    public function getShippingFees()
+    {
+        $shipping_fees = ShippingFee::all();
+
+        return $shipping_fees;
+    }
+
+    //Store Shipping Fee
+    public function storeShippingFee($shipping_fee_details)
+    {
+        $new_shipping_fee_details = [
+            'province_id' => $shipping_fee_details['shipping_province'],
+            'city' => $shipping_fee_details['shipping_city'],
+            'price' => $shipping_fee_details['shipping_price']
+        ];
+        
+        $store_shipping_fee = ShippingFee::create($new_shipping_fee_details);
+
+        return $store_shipping_fee;
+    }
 }
