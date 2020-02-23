3b03ddd735f47a3beea981560a778de9e20d6fd0
ryantesoro <ryantesoro@yahoo.com>
2020-02-23 13:00:15 +0800

    [:hammer_and_wrench:] added super admin middleware to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,12 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        //Super Admin Middleware
+        'super_admin' => \App\Http\Middleware\SuperAdminMiddleware::class,
+
+        //Normal Admin
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 
     /**
