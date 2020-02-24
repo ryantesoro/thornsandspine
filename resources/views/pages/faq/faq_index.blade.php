180bc6bb42d8906e9417adf8dde0af328b054941
ryantesoro <ryantesoro@yahoo.com>
2020-02-24 12:42:55 +0800

    [:lipstick:] updated faq index

--- resources/views/pages/faq/faq_index.blade.php
+++ resources/views/pages/faq/faq_index.blade.php
@@ -9,13 +9,42 @@
         @include('layouts.header')
         <div class="row">
             <div class="col text-left">
-                <h4>List Of Pots</h4>
+                <h4>List Of FAQs</h4>
             </div>
             <div>
-                <a class="btn btn-success font-weight-bold" href="{{ route('admin.pot.create') }}">Create Pot</a>
+                <a class="btn btn-success font-weight-bold" href="{{ route('admin.faq.create') }}">Create Pot</a>
             </div>
         </div>
         <hr>
+        <div id="accordion" role="tablist">
+            @foreach($faqs as $faq)
+            <div class="card">
+                <div class="card-header" role="tab">
+                    <h5 class="mb-0">
+                        <div class="d-flex">
+                            <div class="mr-auto">
+                                <a data-toggle="collapse" href="#question_{{ $faq['id'] }}">
+                                    {{ $faq['question'] }}
+                                </a>
+                            </div>
+
+                            <div class="ml-auto">
+                                <a class="btn btn-sm btn-warning font-weight-bold" href="">
+                                    Edit
+                                </a>
+                            </div>
+                        </div>
+                    </h5>
+                </div>
+
+                <div id="question_{{ $faq['id'] }}" class="collapse" role="tabpanel">
+                    <div class="card-body">
+                        {{ $faq['answer'] }}
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </main>
 </div>
 </div>
