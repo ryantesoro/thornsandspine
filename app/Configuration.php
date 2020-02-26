3df819ec65799d09fc1c70f69af11c0d2f18368d
ryantesoro <ryantesoro@yahoo.com>
2020-02-26 13:22:03 +0800

    [:hammer_and_wrench:] updated function of getConfigurations

--- app/Configuration.php
+++ app/Configuration.php
@@ -20,7 +20,20 @@ class Configuration extends Model
         $configurations = Configuration::select('*')
             ->get();
 
-        return $configurations;
+        $config = [];
+        foreach ($configurations as $configuration) {
+            $config[$configuration->name] = $configuration->value;
+        }
+
+        $card_number = $config['card_number'];
+        $config['card_number_1'] = substr($card_number, 0, 4);
+        $config['card_number_2'] = substr($card_number, 4, 4);
+        $config['card_number_3'] = substr($card_number, 8, 4);
+        $config['card_number_4'] = substr($card_number, 12, 4);
+
+        unset($config['card_number']);
+
+        return $config;
     }
 
     //Update Configuration
--- app/Http/Controllers/WebController/ConfigurationController.php
+++ app/Http/Controllers/WebController/ConfigurationController.php
@@ -13,19 +13,8 @@ class ConfigurationController extends Controller
     public function index()
     {
         $configurations = $this->configuration()->getConfigurations();
-        $config = [];
-        foreach ($configurations as $configuration) {
-            $config[$configuration->name] = $configuration->value;
-        }
         
-        $card_number = $config['card_number'];
-        $config['card_number_1'] = substr($card_number, 0, 4);
-        $config['card_number_2'] = substr($card_number, 4, 4);
-        $config['card_number_3'] = substr($card_number, 8, 4);
-        $config['card_number_4'] = substr($card_number, 12, 4);
-
-        unset($config['card_number']);
-        return view('configuration')->with('configuration', $config);
+        return view('configuration')->with('configuration', $configurations);
     }
 
     public function update(Request $request)
