3e8c133823979283ecc6671351ac44d509f00b8e
ryantesoro <ryantesoro@yahoo.com>
2020-02-27 13:25:49 +0800

    [:hammer_and_wrench:] added deliver and return

--- app/Http/Controllers/WebController/OrderController.php
+++ app/Http/Controllers/WebController/OrderController.php
@@ -20,7 +20,7 @@ class OrderController extends Controller
     public function show(Request $request, $order_code)
     {
         if (!$this->order()->orderExists($order_code)) {
-            Alert::error('Show Order Failed', 'Order does not exist!');
+            Alert::warning('Show Order Failed', 'Order does not exist!');
             return redirect()->route('admin.order.index');
         }
 
@@ -45,12 +45,46 @@ class OrderController extends Controller
         $province = $this->shipping_province()->getProvince($shipping_fee->province_id)->name;
         $shipping_price = $shipping_fee->price;
         $shipping_city_province = ucwords($city.', '.$province);
+        $ss = $order->screenshot()->get();
+        $screenshots = [];
+        foreach ($ss as $screenshot) {
+            $temp_array = [];
+            $temp_array['thumbnail'] = route('image', ["screenshot", $screenshot['file_name'], 'size' => 'thumbnail']);
+            $temp_array['medium'] = route('image', ["screenshot", $screenshot['file_name'], 'size' => 'medium']);
+            $temp_array['original'] = route('image', ["screenshot", $screenshot['file_name']]);
+            $date_uploaded = Carbon::parse($screenshot['created_at'])->format('m-d-Y');
+            $screenshots[$date_uploaded][] = $temp_array;
+        }
 
         return view('pages.order.order_show')
             ->with('order', $order)
             ->with('customer', $customer)
             ->with('products', $products)
             ->with('shipping_price', $shipping_price)
-            ->with('shipping_city_province', $shipping_city_province);
+            ->with('shipping_city_province', $shipping_city_province)
+            ->with('screenshots', $screenshots);
+    }
+
+    public function deliver(Request $request, $order_code)
+    {
+        if (!$this->order()->orderExists($order_code)) {
+            Alert::warning('Show Order Failed', 'Order does not exist!');
+            return redirect()->route('admin.order.index');
+        }
+
+        $order = $this->order()->getOrder($order_code);
+        $update_order = $this->order()->updateOrder(['status' => 2], $order->id);
+        
+        Alert::success('Complete Order Successful', 'Success!');
+        return redirect()->route('admin.order.index');
+    }
+
+    public function return(Request $request, $order_code)
+    {
+        $order = $this->order()->getOrder($order_code);
+        $update_order = $this->order()->updateOrder(['status' => 0, 'comment' => $request->post('comment')], $order->id);
+        
+        Alert::success('Ask Customer Successful', 'Success!');
+        return redirect()->route('admin.order.index');
     }
 }
